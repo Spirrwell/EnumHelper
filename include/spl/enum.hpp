974d24d073280bdc202aeb3a951fc7382b9cdd35
Spirrwell <Spirrwell@gmail.com>
2021-04-26 18:50:27 -0400

    Real initial commit

---
+++ include/spl/enum.hpp
@@ -0,0 +1,82 @@
+/*******************************************************************************
+* MIT License
+*
+* Copyright (c) 2021 Spirrwell
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+********************************************************************************/
+
+#pragma once
+
+#include <type_traits>
+#include <ostream>
+
+template <typename E> requires std::is_enum_v<E>
+constexpr bool operator!(const E &e)
+{
+	return static_cast<typename std::underlying_type<E>::type>(e) == 0;
+}
+
+template <typename E> requires std::is_enum_v<E>
+constexpr E operator~(const E &rhs)
+{
+	return static_cast<E>(~static_cast<typename std::underlying_type<E>::type>(rhs));
+}
+
+template<typename E> requires std::is_enum_v<E>
+constexpr E operator|(const E &lhs, const E &rhs)
+{
+	return static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) | static_cast<typename std::underlying_type<E>::type>(rhs));
+}
+
+template<typename E> requires std::is_enum_v<E>
+constexpr E operator&(const E &lhs, const E &rhs)
+{
+	return static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) & static_cast<typename std::underlying_type<E>::type>(rhs));
+}
+
+template<typename E> requires std::is_enum_v<E>
+constexpr E operator^(const E &lhs, const E &rhs)
+{
+	return static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) ^ static_cast<typename std::underlying_type<E>::type>(rhs));
+}
+
+template<typename E> requires std::is_enum_v<E>
+constexpr E &operator|=(E &lhs, const E &rhs)
+{
+	return reinterpret_cast<E&>(reinterpret_cast<typename std::underlying_type<E>::type&>(lhs) |= static_cast<typename std::underlying_type<E>::type>(rhs));
+}
+
+template<typename E> requires std::is_enum_v<E>
+constexpr E &operator&=(E &lhs, const E &rhs)
+{
+	return reinterpret_cast<E&>(reinterpret_cast<typename std::underlying_type<E>::type&>(lhs) &= static_cast<typename std::underlying_type<E>::type>(rhs));
+}
+
+template<typename E> requires std::is_enum_v<E>
+constexpr E &operator^=(E &lhs, const E &rhs)
+{
+	return reinterpret_cast<E&>(reinterpret_cast<typename std::underlying_type<E>::type&>(lhs) ^= static_cast<typename std::underlying_type<E>::type>(rhs));
+}
+
+template <typename E> requires std::is_enum_v<E>
+inline std::ostream& operator<<(std::ostream &lhs, const E &rhs)
+{
+	return lhs << static_cast<typename std::underlying_type<E>::type>(rhs);
+}
